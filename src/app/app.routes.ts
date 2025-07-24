c26f407d467ddd722f04f778748e456210c99bc2
Nebojsa Ilic <nebojsailic@192.168.1.4>
2025-07-24 09:19:49 +0200

    deleting what is not needed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'todo',
+    redirectTo: 'home',
     pathMatch: 'full',
-  },
-  {
-    path: 'todo',
-    loadChildren: () => import('./modules/todo').then((m) => m.todoRoutes),
-  },
-  {
-    path: 'todo-v2',
-    loadChildren: () => import('./modules/todo-v2').then((m) => m.todoRoutesV2),
   },
   {
     path: '**',
